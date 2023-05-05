b36118f1e05610025e1b73ab60403cf5145e2949
yogeshgarg0011 <125199173+yogeshgarg0011@users.noreply.github.com>
2023-05-05 08:47:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateCharactersCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // --without dictionary-------- -
+            int count = 0;
+            Console.WriteLine("Enter a name::");
+            string name = Console.ReadLine();
+
+            char[] arr = name.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count = 1;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j] && arr[i] != ' ' && arr[j]!='0' )
+                    {
+                        count++;
+                        arr[j] = '0';
+                    }
+                }
+                if (count > 1 )
+                    Console.WriteLine(arr[i] + "---" + count + "times");
+            }
+            Console.ReadLine();
+           
+            
+        }
+    }
+}
